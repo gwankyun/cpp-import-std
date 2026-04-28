0aa7c66c08feff890f36903d2b3dc1dbd6514695
權少 <ljqican@163.com>
2026-04-28 21:34:26 +0800

    一、更新clang複製dll腳本。 二、啟用CTest。

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
 ﻿import std;
 import compiler;
 
+int test()
+{
+#ifdef _MSC_VER
+    if (compiler::id() != "MSVC")
+    {
+        return 1;
+    }
+#elif defined(__clang__)
+    if (compiler::id() != "Clang")
+    {
+        return 1;
+    }
+#else
+    return 1;
+#endif
+
+    return 0;
+}
+
 int main(int _argc, char* _argv[])
 {
     std::println("compiler id     : {}", compiler::id());
     std::println("compiler version: {}", compiler::version());
-    return 0;
+
+    return test();
 }
